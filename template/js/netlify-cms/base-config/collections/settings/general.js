4b02b9129d5499e738148aafebc8ae8f91cf3b90
João Escudero <joaovescudero@gmail.com>
2021-11-26 07:39:43 -0300

    feat(cms): adding theme to cms

--- template/js/netlify-cms/base-config/collections/settings/general.js
+++ template/js/netlify-cms/base-config/collections/settings/general.js
@@ -3,7 +3,7 @@ export default ({ baseDir }) => ({
   label: 'Nome, Cores e Logos da Marca',
   file: `${baseDir}content/settings.json`,
   editor: {
-    preview: false
+    preview: true
   },
   fields: [{
       label: 'Nome da loja',
@@ -30,6 +30,140 @@ export default ({ baseDir }) => ({
       label: 'Cor secundária',
       name: 'secondary_color',
       widget: 'color'
+    },
+    {
+      label: 'Tema',
+      name: 'theme',
+      widget: 'object',
+      fields: [
+        {
+          label: 'Storefront',
+          name: 'custom',
+          widget: 'select',
+          options: [
+            {
+              label: 'Storefront padrão',
+              value: '_'
+            },
+            {
+              label: 'Clean: Branco',
+              value: 'clean-white'
+            },
+            {
+              label: 'Clean: Escala de cinza',
+              value: 'clean-gray'
+            },
+            {
+              label: 'Clean: Preto',
+              value: 'clean-dark'
+            },
+            {
+              label: 'Nicho: Bebê',
+              value: 'niche-baby'
+            },
+            {
+              label: 'Nicho: Flores',
+              value: 'niche-flowers'
+            },
+            {
+              label: 'Nicho: Game',
+              value: 'niche-game'
+            }
+          ]
+        },
+        {
+          label: 'UI base',
+          name: 'bootswatch',
+          hint: 'Powered by https://bootswatch.com/',
+          widget: 'select',
+          options: [
+            {
+              label: 'Bootstrap padrão',
+              value: '_'
+            },
+            {
+              label: 'Cerulean',
+              value: 'cerulean'
+            },
+            {
+              label: 'Cosmo',
+              value: 'cosmo'
+            },
+            {
+              label: 'Cyborg',
+              value: 'cyborg'
+            },
+            {
+              label: 'Darkly',
+              value: 'darkly'
+            },
+            {
+              label: 'Flatly',
+              value: 'flatly'
+            },
+            {
+              label: 'Journal',
+              value: 'journal'
+            },
+            {
+              label: 'Litera',
+              value: 'litera'
+            },
+            {
+              label: 'Lumen',
+              value: 'lumen'
+            },
+            {
+              label: 'Lux',
+              value: 'lux'
+            },
+            {
+              label: 'Materia',
+              value: 'materia'
+            },
+            {
+              label: 'Minty',
+              value: 'minty'
+            },
+            {
+              label: 'Pulse',
+              value: 'pulse'
+            },
+            {
+              label: 'Sandstone',
+              value: 'sandstone'
+            },
+            {
+              label: 'Simplex',
+              value: 'simplex'
+            },
+            {
+              label: 'Sketchy',
+              value: 'sketchy'
+            },
+            {
+              label: 'Slate',
+              value: 'slate'
+            },
+            {
+              label: 'Solar',
+              value: 'solar'
+            },
+            {
+              label: 'Spacelab',
+              value: 'spacelab'
+            },
+            {
+              label: 'United',
+              value: 'united'
+            },
+            {
+              label: 'Yeti',
+              value: 'yeti'
+            }
+          ]
+        }
+      ]
     },
     {
       label: 'Logo',
